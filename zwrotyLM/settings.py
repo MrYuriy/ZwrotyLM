ef72db4cc377bca71ca77d251303629092b12666
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-10-18 07:00:49 +0300

    Add conection ligic to db

--- zwrotyLM/settings.py
+++ zwrotyLM/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
+from decouple import config
 
 load_dotenv()
 
@@ -84,6 +86,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
