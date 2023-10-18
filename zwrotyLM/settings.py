088dacf42914b72b3a20b55cdf46967bf3494e6b
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-10-18 07:52:07 +0300

    Add static files

--- zwrotyLM/settings.py
+++ zwrotyLM/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles/"
+ASSETS_ROOT = "/static/assets"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
